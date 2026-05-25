1eeab41037d9e5ec0e4902ef8047dbaf8bc7b086
HerPerception <offorbuzor1@gmail.com>
2026-05-25 23:48:27 +0100

    Implement TwoSum function using two-pointer technique
    I tried to implement the two-pointer technique to strengthen my knowledge on two-pointer kind of loops in Go. Although this worked, it still needs finishing touches as it only picks the first occurrence.

---
+++ Elementary_Programming/twosum-loop.go
@@ -0,0 +1,25 @@
+// Using two-pointers
+
+package main
+
+import "fmt"
+
+func TwoSum(num []int, target int) ([]int, bool) {
+	newResult := []int{}
+	i, j := 0, len(num)-1
+	for i < j {
+		if num[i]+num[j] == target {
+			newResult = append(newResult, i, j)
+		} else if num[i]+num[j] != target && j > i {
+			j--
+			continue
+		}
+		i++
+		j--
+	}
+	return newResult, len(newResult) != 0
+}
+
+func main() {
+	fmt.Println(TwoSum([]int{-3, 5, 7, 13, 8, 9, 3, 11}, 10))
+}
