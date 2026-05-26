0dc1feba78517110ca212c9a61877ea6000cf7de
HerPerception <offorbuzor1@gmail.com>
2026-05-26 23:55:34 +0100

    Finding the first occurrence of a substring in a string - Leetcode

--- Elementary_Programming/substr.go
+++ Elementary_Programming/substr.go
@@ -1,13 +1,21 @@
 package main
 
-func Substr(str string, substr string)int {
+import "fmt"
+
+func Substr(str string, substr string) int {
 	for i := range str {
-		if i+len(substr) < len(str) && str[0] == substr[0] && str[i:i+len(substr)] == substr{
+		if len(str) == len(substr) && str == substr {
+			return 0
+		}
+		if len(substr) == 1 && string(str[i]) == substr {
+			return i
+		}
+		if i+len(substr) < len(str) && str[0] == substr[0] && str[i:i+len(substr)] == substr {
 			return i
 		}
 	}
 	return -1
 }
 func main() {
-	fmt.Println(Substr("a", "a"))
+	fmt.Println(Substr("ab", "ab"))
 }
